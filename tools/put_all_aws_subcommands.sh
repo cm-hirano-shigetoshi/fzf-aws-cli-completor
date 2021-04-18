ff006b048f8f773653981d4d62cf68de0eb0d050
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2021-04-19 02:00:50 +0900

    全コマンドのsubcommandを出力するスクリプト

---
+++ tools/put_all_aws_subcommands.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -eu
+
+COMMANDS=$(aws help | \
+             fzf -f ^ --ansi | \
+             awk 'BEGIN{p=0} {if (p>0) print; if ($1=="AVAILABLE") p=1}' | \
+             sed -n 's/^.*o \(.*\)$/\1/p' | \
+             head -n -1)
+
+for c in $COMMANDS; do
+    echo $c
+    python $(dirname $0)/get_aws_subcommands.py $c \
+        > $(dirname $0)/../lib/aws_subcommands/aws_$c
+done
+
